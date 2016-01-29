049be54da14615543a9409f77dc225a200c37df8
Sean <taz838@hotmail.co.uk>
2016-01-29 14:26:45 +0000

    removed deprecated classes, updated documentation

--- ctc/CTCCriterion.lua
+++ ctc/CTCCriterion.lua
@@ -13,16 +13,11 @@ function CTCCriterion:__init()
 end
 
 --Calculates and returns the cost of the input respect to the target.
---The input and size of each sequence has to be batched together within a table.
---The input has to be in the format specified at the end of this README:
---https://github.com/baidu-research/warp-ctc/blob/master/torch_binding/TUTORIAL.md
 --Example:
 --Two input activation sequences: {1,2,3,4,5},{6,7,8,9,10} and {11,12,13,14,15},{16,17,18,19,20}
 --Input to the function would be:
---inputData: torch.Tensor({{1,2,3,4,5},{11,12,13,14,15},{6,7,8,9,10},{16,17,18,19,20}})
---sizes would be: {2,2}
---And the final input to the function would be would be: {inputData,sizes}
---target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences).
+--networkOutput: {torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}}),torch.Tensor({{11,12,13,14,15},{16,17,18,19,20}})}
+--target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences as above).
 function CTCCriterion:updateOutput(networkOutput,target)
     local input = convertToCTCBatchSequence(networkOutput)
     local act = torch.FloatTensor(input:size()):copy(input)
@@ -33,17 +28,12 @@ function CTCCriterion:updateOutput(networkOutput,target)
     return self.output
 end
 
---Calculates and returns the gradients in respecto the inputs and targets.
---The input and size of each sequence has to be batched together within a table.
---The input has to be in the format specified at the end of this README:
---https://github.com/baidu-research/warp-ctc/blob/master/torch_binding/TUTORIAL.md
+--Calculates and returns the gradients in respect to the inputs and targets.
 --Example:
 --Two input activation sequences: {1,2,3,4,5},{6,7,8,9,10} and {11,12,13,14,15},{16,17,18,19,20}
 --Input to the function would be:
---inputData: torch.Tensor({{1,2,3,4,5},{11,12,13,14,15},{6,7,8,9,10},{16,17,18,19,20}})
---sizes would be: {2,2}
---And the final input to the function would be would be: {inputData,sizes}
---target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences).
+--networkOutput: {torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}}),torch.Tensor({{11,12,13,14,15},{16,17,18,19,20}})}
+--target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences as above).
 function CTCCriterion:updateGradInput(networkOutput,target)
     local input = convertToCTCBatchSequence(networkOutput)
     local act = torch.FloatTensor(input:size()):copy(input)
