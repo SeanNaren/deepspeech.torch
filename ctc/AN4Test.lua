3d0b9456fab1b80842daea4fbf0b2a96d75a05ff
Sean <seannaren>
2016-02-03 18:41:47 +0000

    updated gitignore, various changes to network layers

--- ctc/AN4Test.lua
+++ ctc/AN4Test.lua
@@ -4,8 +4,8 @@ local Network = require 'Network'
 local an4FolderDir = "/root/CTCSpeechRecognition/Audio/an4"
 local inputs,targets = AudioData.retrieveAN4TrainingDataSet(an4FolderDir)
 local net = Network.createSpeechNetwork()
-local batchSize = 50
-local epochs = 500
+local batchSize = 20
+local epochs = 200
 Network.trainNetwork(net, inputs, targets, batchSize, epochs)
 
 local inputs,targets = AudioData.retrieveAN4TestDataSet(an4FolderDir)
