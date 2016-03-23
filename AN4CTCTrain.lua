c6df56ea169b959cd4bd94d39ef0802bca5212db
Sean <seannaren>
2016-03-23 10:58:17 +0000

    removed testing portion to implement in other script

--- AN4CTCTrain.lua
+++ AN4CTCTrain.lua
@@ -1,32 +1,6 @@
 local AudioData = require 'AudioData'
 local Network = require 'Network'
 
---[[Takes the resulting predictions from the network and prints the letter equivalent in one string.]]
-local function printPredictions(predictions, testSample)
-    local string = ""
-    local prevLetter = ""
-    -- Iterate through the results of the prediction and append the letter that was predicted in the sample.
-    for index, prediction in ipairs(predictions) do
-        local maxValue, maxIndex = torch.max(prediction, 1)
-        -- We minus 1 to the index because a CTC blank has the index of 0.
-        maxIndex = maxIndex[1] - 1
-        -- If the index is 0, that means that the character was blank.
-        if (maxIndex ~= 0) then
-            local letter = AudioData.findLetter(maxIndex)
-            if (letter ~= prevLetter) then
-                string = string .. letter
-                prevLetter = letter
-            end
-        end
-    end
-    local targetSentence = ""
-    for index, characterIndex in ipairs(testSample) do
-        targetSentence = targetSentence .. AudioData.findLetter(characterIndex)
-    end
-    print("Actual target: " .. targetSentence)
-    print("Prediction: " .. string)
-end
-
 --Training parameters
 local epochs = 47
 
@@ -58,23 +32,7 @@ Network:init(networkParams)
 
 Network:trainNetwork(trainingDataSet, epochs, sgdParams)
 
---The test set in spectrogram tensor form.
-local testDataSet = AudioData.retrieveAN4TestDataSet(an4FolderDir, windowSize, stride)
-
---For testing purposes, we predict certain items from the dataset.
---TODO should use a language model or Seq2Seq to handle spell checking.
---TODO this should be an accuracy checker where we loop through all samples to retrieve an accuracy value.
-local numberOfTestSamples = 5
-for i = 0, numberOfTestSamples do
-    local inputs, targets = testDataSet:nextData()
-    local predictions = Network:predict(inputs)
-    printPredictions(predictions, targets[1])
-end
-
 --Creates the loss plot.
 Network:createLossGraph()
 
 print("finished")
-
---prevents script from ending to view loss graph.
-while (true) do end
