74ac86c222e5adcabea5ca01a38089664ecb7c11
Sean <seannaren>
2016-03-27 10:31:40 +0100

    now uses mini-batches

--- AN4CTCTrain.lua
+++ AN4CTCTrain.lua
@@ -2,6 +2,7 @@
 
 local AudioData = require 'AudioData'
 local Network = require 'Network'
+local Batcher = require 'Batcher'
 
 --Training parameters
 local epochs = 47
@@ -25,9 +26,13 @@ local sgdParams = {
 local windowSize = 256
 local stride = 128
 
+--The larger this value, the larger the batches, however the more padding is added to make variable sentences the same.
+local maximumSizeDifference = 0 -- Setting this to zero makes it batch together the same length sentences.
+
 --The training set in spectrogram tensor form.
 local an4FolderDir = "/root/CTCSpeechRecognition/Audio/an4"
-local trainingDataSet = AudioData.retrieveAN4TrainingDataSet(an4FolderDir, windowSize, stride)
+local inputsAndTargets = AudioData.retrieveAN4TrainingDataSet(an4FolderDir, windowSize, stride)
+local trainingDataSet = Batcher.createMinibatchDataset(inputsAndTargets, maximumSizeDifference)
 
 --Create and train the network based on the parameters and training data.
 Network:init(networkParams)
