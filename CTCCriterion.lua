48182989eb9a8390b3d7744f92d643ebacbef4b4
Sean <seannaren>
2016-03-27 10:31:04 +0100

    edited criterion to support batching

--- CTCCriterion.lua
+++ CTCCriterion.lua
@@ -6,46 +6,41 @@ module(..., package.seeall)
 require 'nn'
 require 'cutorch'
 require 'warp_ctc'
+
 local CTCCriterion, parent = torch.class('CTCCriterion', 'nn.Criterion')
 
 function CTCCriterion:__init()
     parent.__init(self)
 end
 
---Calculates and returns the cost of the input respect to the target.
---Example:
---Two input activation sequences: {1,2,3,4,5} and {11,12,13,14,15}
---Input to the function would be:
---networkOutput: {torch.Tensor({1,2,3,4,5}),torch.Tensor({11,12,13,14,15})}
---target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences as above).
-function CTCCriterion:updateOutput(networkOutput, target)
-    local act = CTCCriterion.convertToCTCSequence(networkOutput):cuda()
+function CTCCriterion:updateOutput(output, labels)
+    local tensorSizes = output:size()
+    local acts = CTCCriterion.createCTCBatch(output, tensorSizes)
+    local sizes = {}
+    for x = 1, tensorSizes[1] do
+        table.insert(sizes, tensorSizes[2])
+    end
     local grads = torch.CudaTensor()
-    local labels = target
-    local size = {#networkOutput}
-    self.output = averageCosts(gpu_ctc(act, grads, labels, size))
+    self.output = sumCosts(gpu_ctc(acts, grads, labels, sizes))
     return self.output
 end
 
---Calculates and returns the gradients in respect to the inputs and targets.
---Example:
---Two input activation sequences: {1,2,3,4,5} and {11,12,13,14,15}
---Input to the function would be:
---networkOutput: {torch.Tensor({1,2,3,4,5}),torch.Tensor({11,12,13,14,15})}
---target is the expected labels i.e {{1,2},{3,3}} (for 2 sequences as above).
-function CTCCriterion:updateGradInput(networkOutput, target)
-    local act = CTCCriterion.convertToCTCSequence(networkOutput):cuda()
-    local temp = nn.SoftMax():updateOutput(act:double()):cuda()
-    local grads = temp:clone():zero()
-    local labels = target
-    local size = {#networkOutput}
-    gpu_ctc(act, grads, labels, size)
-    self.gradInput = CTCCriterion.convertToNetSequence(grads:float(), #networkOutput)
+
+function CTCCriterion:updateGradInput(output, labels)
+    local tensorSizes = output:size()
+    local acts = CTCCriterion.createCTCBatch(output, tensorSizes)
+    local sizes = {}
+    for x = 1, tensorSizes[1] do
+        table.insert(sizes, tensorSizes[2])
+    end
+    local grads = acts:clone():zero()
+    gpu_ctc(acts, grads, labels, sizes)
+    self.gradInput = CTCCriterion.revertBatching(grads, tensorSizes)
     return self.gradInput
 end
 
 --If batching occurs multiple costs are returned. We sum the costs and return.
-function averageCosts(list)
+function sumCosts(list)
     local acc
     for k, v in ipairs(list) do
         if 1 == k then
@@ -57,36 +52,30 @@ function averageCosts(list)
     return acc
 end
 
---input to function: {torch.Tensor({1,2,3,4,5}),torch.Tensor({11,12,13,14,15})}
---Returned batched format: torch.Tensor({{1,2,3,4,5},{11,12,13,14,15}})
-function CTCCriterion.convertToCTCSequence(tensors)
-    local columnMajor = {}
-    for index, tensor in ipairs(tensors) do
-        table.insert(columnMajor, torch.totable(tensor))
+-- GPU inputs (preallocate)
+local acts = torch.CudaTensor()
+local convertedGradients = torch.CudaTensor()
+
+function CTCCriterion.createCTCBatch(output, sizes)
+    acts:resize(sizes[1] * sizes[2], sizes[3]):zero()
+    local counter = 1
+    for i = 1, sizes[2] do
+        for j = 1, sizes[1] do
+            acts[counter] = output[j][i]
+            counter = counter + 1
+        end
     end
-    local resultTensor = torch.Tensor(columnMajor)
-    return resultTensor
+    return acts
 end
 
---Reverses the batching process to give the gradientOutput for
---backwards propagation on the net.
---Example:
---input to function (output of the CTCCriterion):
---torch.Tensor({{1,2,3,4,5},{11,12,13,14,15},{6,7,8,9,10},{0,0,0,0,0}})
---Returned format:
---{torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}}),torch.Tensor({{11,12,13,14,15},{0,0,0,0,0}})}
-function CTCCriterion.convertToNetSequence(gradientOutput, numberOfSequences)
-    local gradients = {}
-    for i = 1, gradientOutput:size(1) do
-        local index = math.fmod(i, numberOfSequences)
-        if (index == 0) then index = numberOfSequences end
-        gradients[index] =  torch.totable(gradientOutput[i])
+function CTCCriterion.revertBatching(gradients, sizes)
+    convertedGradients:resize(sizes[1] ,sizes[2], sizes[3]):zero()
+    local counter = 1
+    for i = 1, sizes[2] do
+        for j = 1, sizes[1] do
+            convertedGradients[j][i] = gradients[counter]
+            counter = counter + 1
+        end
     end
-    local returnTensors = {}
-    for i = 1, numberOfSequences do
-        table.insert(returnTensors, torch.CudaTensor(gradients[i]))
-    end
-    return returnTensors
+    return convertedGradients
 end
-
-return CTCCriterion
--- CTCCriterionTest.lua
+++ CTCCriterionTest.lua
@@ -3,47 +3,50 @@
 require 'cunn'
 require 'optim'
 require 'rnn'
+require 'CTCCriterion'
 
-
-local CTCCriterion = require 'CTCCriterion'
+local CTCCriterion = CTCCriterion()
 
 function smallTest()
-    local acts = {torch.Tensor({0,0,0,0,0}):cuda()}
+    local acts = torch.Tensor({{{0,0,0,0,0}}}):cuda()
     local targets = {{1}}
     assertion(CTCCriterion:updateOutput(acts,targets), 1.6094379425049, "CTCCriterion.smallTest")
 end
 
 function mediumTest()
-    local acts = {
-        torch.Tensor({1,2,3,4,5}):cuda(),
-        torch.Tensor({6,7,8,9,10}):cuda(),
-        torch.Tensor({11,12,13,14,15}):cuda()
-    }
+    local acts =
+        torch.Tensor({
+            {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}}
+        }):cuda()
+
     local targets = {{3,3}}
     assertion(CTCCriterion:updateOutput(acts,targets), 7.355742931366, "CTCCriterion.mediumTest")
 end
 
 function mediumNegativeTest()
-    local acts = {
-        torch.Tensor({-5,-4,-3,-2,-1}):cuda(),
-        torch.Tensor({-10,-9,-8,-7,-6}):cuda(),
-        torch.Tensor({-15,-14,-13,-12,-11}):cuda()
-    }
+    local acts =
+        torch.Tensor({
+            {{-5,-4,-3,-2,-1}, {-10,-9,-8,-7,-6}, {-15,-14,-13,-12,-11}}
+            }):cuda()
+
     local targets = {{2,3}}
     assertion(CTCCriterion:updateOutput(acts,targets), 4.938850402832, "CTCCriterion.mediumNegativeTest")
 end
 
--- TODO batching is currently not supported, hopefully soon.
+
 -- The batch test takes the three above tests and combines them into one batch.
--- because of the CTCBatcher we have to pad the tensor of {{0,0,0,0}} to the max length tensor in the batch.
+-- The batch sequences must be of the same length (length of 3 below) (as a result the test results are slightly
+-- different to the Torch CTC binding tutorial README due to the first column also having 3 elements rather than 1.)
 function batchTest()
---    local acts = {
---        torch.Tensor({{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}}):float(),
---        torch.Tensor({{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}):float(),
---        torch.Tensor({{-5,-4,-3,-2,-1},{-10,-9,-8,-7,-6},{-15,-14,-13,-12,-11}}):float()
---    }
---    local targets = {{1},{3,3},{2,3}}
---    assertion(CTCCriterion:updateOutput(acts,targets), 5.1103823979696, "CTCCriterion.batchTest")
+    local acts =
+        torch.Tensor({
+            {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}},
+            {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}},
+            {{-5,-4,-3,-2,-1},{-10,-9,-8,-7,-6},{-15,-14,-13,-12,-11}}
+            }):cuda()
+
+    local targets = {{1},{3,3},{2,3}}
+    assertion(CTCCriterion:updateOutput(acts,targets), 15.331147670746, "CTCCriterion.batchTest")
 end
 
 function assertion(outputCost,expectedCost,testName)
@@ -54,22 +57,24 @@ function assertion(outputCost,expectedCost,testName)
     end
 end
 
--- Tests are modelled around examples given in the CTCBatcher class
-
-function convertToCTCSequenceTest()
-    local input = {torch.Tensor({1,2,3,4,5}),torch.Tensor({6,7,8,9,10})}
-    local output = CTCCriterion.convertToCTCSequence(input)
-    local expected = torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}})
-    assertion(output, expected, "convertToCTCBatchSequenceTest")
+-- Takes a 3D tensor of batch x time x freq and converts this into the 2d batch tensor form.
+function createCTCBatchTest()
+    local input = torch.Tensor({{{1,2,3,4,5}},{{6,7,8,9,10}}})
+    local output = CTCCriterion.createCTCBatch(input, input:size())
+    local expected = torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}}):cuda()
+    assertion(output, expected, "createCTCBatchTest")
 end
 
-function convertToNetSequenceTest()
-    local input = torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}})
-    local numberOfSequences = 2
-    local output = CTCCriterion.convertToNetSequence(input,numberOfSequences)
-    local expected = {torch.Tensor({1,2,3,4,5}):cuda(),torch.Tensor({6,7,8,9,10}):cuda()}
-    assertion(output[1], expected[1], "convertToNetSequenceTest 1st output")
-    assertion(output[2], expected[2], "convertToNetSequenceTest 2nd output")
+-- Reverts the 2d batch tensor form from CTC into a 3d tensor of batch x time x freq
+function revertBatchingTest()
+    local input = torch.Tensor({{{1,2,3,4,5}},{{6,7,8,9,10}}})
+    local gradient = torch.Tensor({{1,2,3,4,5},{6,7,8,9,10}})
+
+    local output = CTCCriterion.revertBatching(gradient, input:size())
+
+    local expected = torch.Tensor({{{1,2,3,4,5}},{{6,7,8,9,10}}}):cuda()
+    assertion(output[1], expected[1], "revertBatchingTest 1st output")
+    assertion(output[2], expected[2], "revertBatchingTest 2nd output")
 end
 
 function assertion(output, expected,testName)
@@ -86,5 +91,5 @@ mediumNegativeTest()
 batchTest()
 
 --CTC sequencing tests.
-convertToCTCSequenceTest()
-convertToNetSequenceTest()
+createCTCBatchTest()
+revertBatchingTest()
