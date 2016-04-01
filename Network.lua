74df3d042e0b62cbbf5313ff9701208a085957cd
Sean <seannaren>
2016-04-01 23:19:46 +0100

    introduced new rnn, added history return on training, changes to network

--- Network.lua
+++ Network.lua
@@ -5,10 +5,14 @@ require 'cudnn'
 require 'CTCCriterion'
 require 'optim'
 require 'rnn'
+require 'TorchLSTM'
+require 'ReverseSequence'
+require 'Linear3D'
+require 'TemporalBatchNormalization'
 require 'gnuplot'
 require 'xlua'
-local Network = {}
 
+local Network = {}
 local logger = optim.Logger('train.log')
 logger:setNames { 'loss' }
 logger:style { '-' }
@@ -31,8 +35,22 @@ end
 function Network:createSpeechNetwork()
     torch.manualSeed(123)
 
-    local cnn = nn.Sequential()
+    local function BRNN(inputDim, outputDim)
+        local fwd = nn.TorchLSTM(inputDim, outputDim)
+        local bwd = nn.Sequential()
+        bwd:add(nn.ReverseSequence(2))
+        bwd:add(nn.TorchLSTM(inputDim, outputDim))
+        bwd:add(nn.ReverseSequence(2))
 
+        local BiDirectional = nn.Concat(3)
+        BiDirectional:add(fwd)
+        BiDirectional:add(bwd)
+        return BiDirectional
+    end
+
+    local model = nn.Sequential()
+
+    local cnn = nn.Sequential()
     cnn:add(cudnn.SpatialConvolution(1, 32, 41, 11, 2, 2))
     cnn:add(cudnn.SpatialBatchNormalization(32))
     cnn:add(cudnn.ReLU(true))
@@ -46,40 +64,23 @@ function Network:createSpeechNetwork()
     -- batchsize x featuremap x freq x time
     cnn:add(nn.SplitTable(1))
     -- features x freq x time
+
     cnn:add(nn.Sequencer(nn.View(1, 32 * 25, -1)))
     -- batch x features x time
     cnn:add(nn.JoinTable(1))
     -- batch x time x features
     cnn:add(nn.Transpose({ 2, 3 }))
 
-
-    local model = nn.Sequential()
+    -- b x t x f
     model:add(cnn)
-    model:add(nn.Transpose({ 1, 2 }))
-    model:add(nn.SplitTable(1))
-    model:add(nn.Sequencer(nn.Linear(32 * 25, 512)))
-    model:add(nn.Sequencer(nn.BatchNormalization(512)))
-    model:add(nn.Sequencer(nn.ReLU(true)))
-    model:add(nn.Sequencer(nn.Dropout(0.5)))
-    model:add(createBiDirectionalNetwork())
-    model:add(nn.Sequencer(nn.BatchNormalization(200)))
-    model:add(nn.Sequencer(nn.Linear(200, 28)))
-    model:add(nn.Sequencer(nn.View(1, -1, 28)))
-    model:add(nn.JoinTable(1))
-    model:add(nn.Transpose({ 1, 2 }))
+    model:add(BRNN(32 * 25, 200))
+    model:add(nn.TemporalBatchNormalization(400))
+    model:add(BRNN(400, 200))
+    model:add(nn.TemporalBatchNormalization(400))
+    model:add(nn.Linear3D(400, 28))
     model:cuda()
+    model:training()
     Network.model = model
-end
-
--- Creates the stack of bi-directional RNNs.
-function createBiDirectionalNetwork()
-    local biseqModel = nn.Sequential()
-    biseqModel:add(nn.BiSequencer(nn.FastLSTM(512, 100)))
-    biseqModel:add(nn.BiSequencer(nn.FastLSTM(200, 100)))
-    biseqModel:add(nn.BiSequencer(nn.FastLSTM(200, 100)))
-    biseqModel:add(nn.BiSequencer(nn.FastLSTM(200, 100)))
-    biseqModel:add(nn.BiSequencer(nn.FastLSTM(200, 100)))
-    return biseqModel;
 end
 
 -- Returns a prediction of the input net and input tensors.
@@ -91,7 +92,8 @@ end
 --Trains the network using SGD and the defined feval.
 --Uses warp-ctc cost evaluation.
 function Network:trainNetwork(dataset, epochs, sgd_params)
-    local ctcCriterion = CTCCriterion()
+    local history = {}
+    local ctcCriterion = nn.CTCCriterion()
     local x, gradParameters = Network.model:getParameters()
 
     -- GPU inputs (preallocate)
@@ -107,6 +109,9 @@ function Network:trainNetwork(dataset, epochs, sgd_params)
         Network.model:zeroGradParameters()
         local gradOutput = ctcCriterion:backward(predictions, targets)
         Network.model:backward(inputs, gradOutput)
+
+        if (loss < 0) then print("LOSS", loss, " size of input: ", inputs:size(), " size of predictions", predictions:size()) end
+
         return loss, gradParameters
     end
 
@@ -117,8 +122,6 @@ function Network:trainNetwork(dataset, epochs, sgd_params)
         local averageLoss = 0
         print(string.format("Training Epoch: %d", i))
         for j = 1, dataSetSize do
-            collectgarbage()
-            cutorch.synchronize()
             currentLoss = 0
             local _, fs = optim.sgd(feval, x, sgd_params)
             currentLoss = currentLoss + fs[1]
@@ -127,21 +130,26 @@ function Network:trainNetwork(dataset, epochs, sgd_params)
         end
         averageLoss = averageLoss / dataSetSize -- Calculate the average loss at this epoch.
         logger:add { averageLoss } -- Add the average loss value to the logger.
+        table.insert(history, averageLoss) -- Add the average loss value to the logger.
         print(string.format("Training Epoch: %d Average Loss: %f", i, averageLoss))
     end
     local endTime = os.time()
     local secondsTaken = endTime - startTime
-    print("Minutes taken to train: ", secondsTaken / 60)
+    local minutesTaken = secondsTaken / 60
+    print("Minutes taken to train: ", minutesTaken)
 
     if (Network.saveModel) then
         print("Saving model")
         Network:saveNetwork(Network.fileName)
     end
+    return history, minutesTaken
 end
+
 
 function Network:createLossGraph()
     logger:plot()
 end
+
 
 function Network:saveNetwork(saveName)
     torch.save(saveName, Network.model)
