89dbac3dd3eb1465e5be6eb3c81133c5090c67b9
SeanNaren <taz838@hotmail.co.uk>
2016-04-03 20:51:12 +0100

    changed  to call self object

--- Network.lua
+++ Network.lua
@@ -23,11 +23,11 @@ function Network:init(networkParams)
     self.saveModel = networkParams.saveModel or false -- Set to true if you want to save the model after training.
     self.fileName = networkParams.fileName -- The file name to save/load the network from.
     self.model = nil
-    if (Network.loadModel) then
+    if (self.loadModel) then
         assert(networkParams.fileName, "Filename hasn't been given to load model.")
-        Network:loadNetwork(networkParams.fileName)
+        self:loadNetwork(networkParams.fileName)
     else
-        Network:createSpeechNetwork()
+        self:createSpeechNetwork()
     end
     assert((networkParams.saveModel or networkParams.loadModel) and networkParams.fileName, "To save/load you must specify the fileName you want to save to")
 end
@@ -68,12 +68,12 @@ function Network:createSpeechNetwork()
     model:add(nn.Linear3D(400, 28))
     model:cuda()
     model:training()
-    Network.model = model
+    self.model = model
 end
 
 -- Returns a prediction of the input net and input tensors.
 function Network:predict(inputTensors)
-    local prediction = Network.model:forward(inputTensors)
+    local prediction = self.model:forward(inputTensors)
     return prediction
 end
 
@@ -82,7 +82,7 @@ end
 function Network:trainNetwork(dataset, epochs, sgd_params)
     local history = {}
     local ctcCriterion = nn.CTCCriterion():cuda()
-    local x, gradParameters = Network.model:getParameters()
+    local x, gradParameters = self.model:getParameters()
 
     -- GPU inputs (preallocate)
     local inputs = torch.CudaTensor()
@@ -92,14 +92,11 @@ function Network:trainNetwork(dataset, epochs, sgd_params)
         -- transfer over to GPU
         inputs:resize(inputsCPU:size()):copy(inputsCPU)
         gradParameters:zero()
-        local predictions = Network.model:forward(inputs)
+        local predictions = self.model:forward(inputs)
         local loss = ctcCriterion:forward(predictions, targets)
-        Network.model:zeroGradParameters()
+        self.model:zeroGradParameters()
         local gradOutput = ctcCriterion:backward(predictions, targets)
-        Network.model:backward(inputs, gradOutput)
-
-        if (loss < 0) then print("LOSS", loss, " size of input: ", inputs:size(), " size of predictions", predictions:size()) end
-
+        self.model:backward(inputs, gradOutput)
         return loss, gradParameters
     end
 
@@ -126,9 +123,9 @@ function Network:trainNetwork(dataset, epochs, sgd_params)
     local minutesTaken = secondsTaken / 60
     print("Minutes taken to train: ", minutesTaken)
 
-    if (Network.saveModel) then
+    if (self.saveModel) then
         print("Saving model")
-        Network:saveNetwork(Network.fileName)
+        self:saveNetwork(self.fileName)
     end
     return history, minutesTaken
 end
@@ -140,13 +137,13 @@ end
 
 
 function Network:saveNetwork(saveName)
-    torch.save(saveName, Network.model)
+    torch.save(saveName, self.model)
 end
 
 --Loads the model into Network.
 function Network:loadNetwork(saveName)
     local model = torch.load(saveName)
-    Network.model = model
+    self.model = model
     model:evaluate()
 end
 
