47c2807db9ce684bb97fb5407f9547a2965a56e0
SeanNaren <taz838@hotmail.co.uk>
2016-04-26 23:16:38 +0100

    Added spacing

--- DeepSpeechModel.lua
+++ DeepSpeechModel.lua
@@ -13,17 +13,17 @@ local function deepSpeech(GRU)
     model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
     model:add(nn.CombineDimensions(2, 3)) -- Combine the middle two dimensions from 4D to 3D (features x batch x seqLength)
-    model:add(nn.Transpose({1,3})) -- Transpose till seqLength x batch x features
+    model:add(nn.Transpose({1, 3})) -- Transpose till seqLength x batch x features
 
     model:add(nn.TemporalBatchNormalization(32 * 25))
     if (GRU) then
-        model:add(cudnn.BGRU(32 * 25, 200, 2))
+        model:add(cudnn.BGRU(32 * 25, 400, 2))
     else
-        model:add(cudnn.BLSTM(32 * 25, 200, 2))
+        model:add(cudnn.BLSTM(32 * 25, 400, 2))
     end
-    model:add(nn.Transpose({1,2})) -- batch x seqLength x features
-    model:add(nn.MergeConcat(200,3)) -- Sums the outputDims of the two outputs layers from BRNN into one.
-    model:add(nn.TemporalBatchNormalization(200))
+    model:add(nn.Transpose({1, 2})) -- batch x seqLength x features
+    model:add(nn.MergeConcat(400, 3)) -- Sums the outputDims of the two outputs layers from BRNN into one.
+    model:add(nn.TemporalBatchNormalization(400))
     model:add(nn.Linear3D(200, 28))
     return model
 end
