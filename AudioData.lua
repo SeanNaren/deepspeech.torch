9667c536cf3662ddcf4d0067efc4094237a640de
SeanNaren <taz838@hotmail.co.uk>
2016-04-27 10:09:11 +0100

    Added better regex of AN4 set, combined dataset with transcripts

--- AudioData.lua
+++ AudioData.lua
@@ -20,84 +20,21 @@ function AudioData.findLetter(index)
     return indexMapping[index]
 end
 
-function AudioData.retrieveAN4TrainingDataSet(folderDirPath, windowSize, stride)
-    local audioLocationPath = folderDirPath .. "/etc/an4_train.fileids"
-    local transcriptPath = folderDirPath .. "/etc/an4_train.transcription"
-    local nbSamples = 948 -- Amount of samples found in the AN4 training set.
-
-    local targets = {}
-
-    for line in io.lines(transcriptPath) do
-        local label = {}
-        line = string.lower(line)
-        for string in string.gmatch(line, ">([^<]*)<") do
-            --This line removes the space at the beginning and end of the sentence input.
-            string = string:sub(2):sub(1, -2)
-            for i = 1, #string do
-                local character = string:sub(i, i)
-                table.insert(label, alphabetMapping[character])
-            end
-            table.insert(targets, label)
-        end
+local function convertLineToLabels(labels, transcripts, line)
+    local label = {}
+    line = string.lower(line)
+    -- Remove: beginning space, BOS, EOS, fileid, final space, string ids (<s> and </s>).
+    line = line:gsub('^%s', ''):gsub('', ''):gsub('', ''):gsub('%(.+%)', ''):gsub('%s$', ''):gsub('<s>', ''):gsub('</s>', '')
+    --Remove the space at the end of the line.
+    for i = 1, #line do
+        local character = line:sub(i, i)
+        table.insert(label, alphabetMapping[character])
     end
-    local dataSet = an4Dataset(folderDirPath, audioLocationPath, windowSize, stride, targets, nbSamples)
-    return dataSet
+    table.insert(labels, label)
+    table.insert(transcripts, line)
 end
 
-function AudioData.retrieveAN4TestDataSet(folderDirPath, windowSize, stride)
-    local audioLocationPath = folderDirPath .. "/etc/an4_test.fileids"
-    local transcriptPath = folderDirPath .. "/etc/an4_test.transcription"
-    local nbSamples = 130 -- Amount of samples found in the AN4 test set.
-    local targets = {}
-    local transcripts = {} -- For verification of words, we return the lines of the test data.
-
-    for line in io.lines(transcriptPath) do
-        local label = {}
-        line = string.lower(line)
-        local line = line:gsub('%b()', '')
-        --Remove the space at the end of the line.
-        line = line:sub(1, -2)
-        for i = 1, #line do
-            local character = line:sub(i, i)
-            table.insert(label, alphabetMapping[character])
-        end
-        table.insert(targets, label)
-        table.insert(transcripts, line)
-    end
-    local dataSet = an4Dataset(folderDirPath, audioLocationPath, windowSize, stride, targets, nbSamples)
-    return dataSet, transcripts
-end
-
---Used by the Seq2Seq model. Retrieves both the test and training transcripts for AN4.
-function AudioData.retrieveAN4TranscriptSet(folderDirPath)
-    local transcriptPathTrain = folderDirPath .. "/etc/an4_train.transcription"
-    local transcriptPathTest = folderDirPath .. "/etc/an4_test.transcription"
-
-    local trainingTranscripts = {}
-    local testTranscripts = {}
-
-    local function convertToLabel(transcripts, line)
-        local label = {}
-        local string = line:gsub('%b()', '')
-        --Remove the space at the end of the line.
-        string = string:sub(1, -2)
-        for i = 1, #string do
-            local character = string:sub(i, i)
-            table.insert(label, alphabetMapping[character])
-        end
-        table.insert(transcripts, label)
-    end
-
-    for line in io.lines(transcriptPathTrain) do
-        convertToLabel(trainingTranscripts, line)
-    end
-    for line in io.lines(transcriptPathTest) do
-        convertToLabel(testTranscripts, line)
-    end
-    return trainingTranscripts
-end
-
-function an4Dataset(folderDirPath, audioLocationPath, windowSize, stride, targets, nbOfSamples)
+local function an4Dataset(folderDirPath, audioLocationPath, targets, transcripts, windowSize, stride, nbOfSamples)
     local inputs = {}
     local counter = 0
     for audioPath in io.lines(audioLocationPath) do
@@ -108,16 +45,44 @@ function an4Dataset(folderDirPath, audioLocationPath, windowSize, stride, target
         table.insert(inputs, spectrogram)
         xlua.progress(counter, nbOfSamples)
     end
-    local inputsAndTargets = combineInputsAndTargets(inputs, targets)
+    local inputsAndTargets = combineInputsAndTargets(inputs, targets, transcripts)
     return inputsAndTargets
 end
 
-function combineInputsAndTargets(inputs, targets)
+local function combineInputsAndTargets(inputs, targets, transcripts)
     local inputsAndTargets = {}
     for i = 1, #inputs do
-        table.insert(inputsAndTargets, { tensor = inputs[i], label = targets[i] })
+        table.insert(inputsAndTargets, { tensor = inputs[i], label = targets[i], text = transcripts[i] })
     end
     return inputsAndTargets
+end
+
+function AudioData.retrieveAN4TrainingDataSet(folderDirPath, windowSize, stride)
+    local audioLocationPath = folderDirPath .. "/etc/an4_train.fileids"
+    local transcriptPath = folderDirPath .. "/etc/an4_train.transcription"
+    local nbSamples = 948 -- Amount of samples found in the AN4 training set.
+    local transcripts = {}
+    local targets = {}
+
+    for line in io.lines(transcriptPath) do
+        convertLineToLabels(targets, transcripts, line)
+    end
+    local dataSet = an4Dataset(folderDirPath, audioLocationPath, targets, transcripts, windowSize, stride, nbSamples)
+    return dataSet, transcripts
+end
+
+function AudioData.retrieveAN4TestDataSet(folderDirPath, windowSize, stride)
+    local audioLocationPath = folderDirPath .. "/etc/an4_test.fileids"
+    local transcriptPath = folderDirPath .. "/etc/an4_test.transcription"
+    local nbSamples = 130 -- Amount of samples found in the AN4 test set.
+    local targets = {}
+    local transcripts = {} -- For verification of words, we return the lines of the test data.
+
+    for line in io.lines(transcriptPath) do
+        convertLineToLabels(targets, transcripts, line)
+    end
+    local dataSet = an4Dataset(folderDirPath, audioLocationPath, targets, transcripts, windowSize, stride, nbSamples)
+    return dataSet, transcripts
 end
 
 return AudioData
