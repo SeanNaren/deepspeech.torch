aa285da4113e083c8913bd8ef5eb7359439eaa24
SeanNaren <taz838@hotmail.co.uk>
2016-04-29 17:36:05 +0100

    Removed temporal batch norm

--- DeepSpeechModel.lua
+++ DeepSpeechModel.lua
@@ -13,10 +13,11 @@ local function deepSpeech(GRU)
     model:add(cudnn.ReLU(true))
     model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
-    model:add(nn.SplitTable(1)) -- batchsize x featuremap x freq x seqLength
-    model:add(nn.Sequencer(nn.View(1, 32 * 25, -1))) -- features x freq x seqLength
-    model:add(nn.JoinTable(1)) -- batch x features x seqLength
-    model:add(nn.Transpose({ 1, 3 }, { 2, 3 })) -- seqLength x batch x features
+    model:add(nn.SplitTable(1)) -- batchsize x featuremap x freq x time
+    model:add(nn.Sequencer(nn.View(1, 32 * 25, -1))) -- features x freq x time
+    model:add(nn.JoinTable(1)) -- batch x features x time
+    model:add(nn.Transpose({ 2, 3 })) -- batch x time x features
+    model:add(nn.Transpose({ 1, 2 })) -- time x batch x features
 
     if (GRU) then
         model:add(cudnn.BGRU(32 * 25, 400, 4))
@@ -24,13 +25,8 @@ local function deepSpeech(GRU)
         model:add(cudnn.BLSTM(32 * 25, 400, 4))
     end
 
-    model:add(nn.Transpose({ 1, 2 })) -- batch x seqLength x features * 2
-    model:add(nn.MergeConcat(400, 3)) -- batch x seqLength x features
-
-    model:add(nn.Transpose({2, 3})) -- batch x features x seqLength
-    model:add(nn.TemporalBatchNormalization(400)) -- Keep a running mean on the features dim.
-    model:add(nn.Transpose({2, 3})) -- batch x seqLength x features
-
+    model:add(nn.Transpose({ 1, 2 })) -- batch x seqLength x features
+    model:add(nn.MergeConcat(400, 3)) -- Sums the outputDims of the two outputs layers from BRNN into one.
     model:add(nn.Linear3D(400, 28))
     return model
 end
