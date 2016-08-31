0a26cfa3d6c164e3589d20dddd5c067ae9cb6942
SeanNaren <seannaren@hotmail.com>
2016-08-31 17:52:50 +0100

    Added check for an4 already downloaded

--- prepare_an4/prepare.sh
+++ prepare_an4/prepare.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 chmod u+rx ./ConvertAN4ToWav.sh ./generateIndices.sh
-wget http://www.speech.cs.cmu.edu/databases/an4/an4_raw.bigendian.tar.gz
-tar -xzvf an4_raw.bigendian.tar.gz
+if [ ! -d an4 ]; then
+    wget http://www.speech.cs.cmu.edu/databases/an4/an4_raw.bigendian.tar.gz
+    tar -xzvf an4_raw.bigendian.tar.gz
+fi
 rm -r an4_raw.bigendian.tar.gz
 ln -s ../Mapper.lua .
+ln -s ../Lexicon.lua .
 AN4_PATH='an4'
 echo "ROOT_FOLDER: $AN4_PATH"
 find $AN4_PATH -name '*.wav' -delete
