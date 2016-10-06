cd9968ca9960664ef0b07f6a4fb81db9a6404a64
SeanNaren <seannaren@hotmail.com>
2016-10-06 07:44:31 +0100

    Changes to support larger datasets

--- MakeLMDB.lua
+++ MakeLMDB.lua
@@ -41,60 +41,69 @@ local function closeWriter(db, txn)
 end
 
 local function createLMDB(dataPath, lmdbPath)
-
-    local size = tonumber(sys.execute("find " .. dataPath .. " -type f -name '*'" .. extension .. " | wc -l "))
-
     local vecs = tds.Vec()
-    vecs:resize(size)
 
-    local files = io.popen("find -L " .. dataPath .. " -type f -name '*" .. extension .. "'")
-    local counter = 1
-    print("Retrieving sizes for sorting...")
-    local buffer = tds.Vec()
-    buffer:resize(size)
-
-    for file in files:lines() do
-        buffer[counter] = file
-        counter = counter + 1
-    end
-    local function getSize(opts)
-        local audioFilePath = opts.file
-        local transcriptFilePath = opts.file:gsub(opts.extension, ".txt")
-        local opt = opts.opt
-        local audioFile = audio.load(audioFilePath)
-        local length = audio.spectrogram(audioFile, opt.windowSize * opt.sampleRate, 'hamming', opt.stride * opt.sampleRate):size(2)
-        return { audioFilePath, transcriptFilePath, length }
+    local function file_exists(name)
+        local f = io.open(name, "r")
+        if f ~= nil then io.close(f) return true else return false end
     end
 
-    for x = 1, opt.processes do
-        local opts = { extension = extension, file = buffer[x], opt = opt }
-        parallel.children[x]:send({ opts, getSize })
-    end
+    if not file_exists('sort_ids.t7') then
+        local size = tonumber(sys.execute("find " .. dataPath .. " -type f -name '*'" .. extension .. " | wc -l "))
+        vecs:resize(size)
 
-    counter = 1
-    local processCounter = 1
-    for x = 1, size do
-        local result = parallel.children[processCounter]:receive()
-        vecs[counter] = tds.Vec(unpack(result))
-        xlua.progress(counter, size)
-        counter = counter + 1
-        if counter % 5000 == 0 then collectgarbage() end
-        -- send next index to retrieve
-        if x + opt.processes <= size then
-            local opts = { extension = extension, file = buffer[x + opt.processes], opt = opt }
-            parallel.children[processCounter]:send({ opts, getSize })
+        local files = io.popen("find -L " .. dataPath .. " -type f -name '*" .. extension .. "'")
+        local counter = 1
+        print("Retrieving sizes for sorting...")
+        local buffer = tds.Vec()
+        buffer:resize(size)
+
+        for file in files:lines() do
+            buffer[counter] = file
+            counter = counter + 1
         end
-        if processCounter == opt.processes then
-            processCounter = 1
-        else
-            processCounter = processCounter + 1
-        end
-    end
-    print("Sorting...")
-    -- sort the files by length
-    local function comp(a, b) return a[3] < b[3] end
 
-    vecs:sort(comp)
+        local function getSize(opts)
+            local audioFilePath = opts.file
+            local transcriptFilePath = opts.file:gsub(opts.extension, ".txt")
+            local opt = opts.opt
+            local audioFile = audio.load(audioFilePath)
+            local length = audio.spectrogram(audioFile, opt.windowSize * opt.sampleRate, 'hamming', opt.stride * opt.sampleRate):size(2)
+            return { audioFilePath, transcriptFilePath, length }
+        end
+
+        for x = 1, opt.processes do
+            local opts = { extension = extension, file = buffer[x], opt = opt }
+            parallel.children[x]:send({ opts, getSize })
+        end
+
+        local processCounter = 1
+        for x = 1, size do
+            local result = parallel.children[processCounter]:receive()
+            vecs[x] = tds.Vec(unpack(result))
+            xlua.progress(x, size)
+            if x % 1000 == 0 then collectgarbage() end
+            -- send next index to retrieve
+            if x + opt.processes <= size then
+                local opts = { extension = extension, file = buffer[x + opt.processes], opt = opt }
+                parallel.children[processCounter]:send({ opts, getSize })
+            end
+            if processCounter == opt.processes then
+                processCounter = 1
+            else
+                processCounter = processCounter + 1
+            end
+        end
+        print("Sorting...")
+        -- sort the files by length
+        local function comp(a, b) return a[3] < b[3] end
+
+        vecs:sort(comp)
+        torch.save('sort_ids.t7', vecs)
+    else
+        vecs = torch.load('sort_ids.t7')
+    end
+    local size = #vecs
 
     print("Creating LMDB dataset to: " .. lmdbPath)
     -- start writing
@@ -137,9 +146,11 @@ local function createLMDB(dataPath, lmdbPath)
         readerTrans:put(x, transcript)
 
         -- commit buffer
-        if x % 5000 == 0 then
+        if x % 1000 == 0 then
             readerSpect:commit(); readerSpect = dbSpect:txn()
             readerTrans:commit(); readerTrans = dbTrans:txn()
+        end
+        if x % 5000 == 0 then
             collectgarbage()
         end
 
@@ -168,6 +179,7 @@ function parent()
             local object = parallel.parent:receive()
             local opts, code = unpack(object)
             local result = code(opts)
+            collectgarbage()
             parallel.parent:send(result)
         end
     end
